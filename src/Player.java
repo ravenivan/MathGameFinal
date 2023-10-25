a95bc48829248f8d4a511cecb5717336a17d06b9
BT_2N2_06 <sample@bths.edu>
2023-10-25 14:04:02 -0400

    final done

--- src/Player.java
+++ src/Player.java
@@ -2,10 +2,14 @@ public class Player {
 
     private String name;
     private int score;
+    private int winStreak;
+    private int highestWinStreak;
 
     public Player(String name) {
         this.name = name;
         score = 0;
+        winStreak = 0;
+        highestWinStreak = 0;
     }
 
     public int getScore() {
@@ -16,11 +20,23 @@ public class Player {
         return name;
     }
 
+    public int getWinStreak() {return winStreak;}
+
+    public int getHighestWinStreak() { return highestWinStreak;}
+
     public void incrementScore() {
         score++;
     }
 
-    public void reset() {
-        score = 0;
+    public void reset() {score = 0;}
+
+    public void resetWinSteak() { winStreak = 0;}
+
+    public void incrementWinSteak() { winStreak++; }
+
+    public void setHighestWinStreak() {
+        if (winStreak > highestWinStreak) {
+            highestWinStreak = winStreak;
+        }
     }
 }
